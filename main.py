ca93ef97b91cf85d3bccbd220ede5fbdf15923ed
Hemanth Vudumula <vudumulahemanth@gmail.com>
2024-12-18 18:29:43 +0530

    removed logs statements

--- main.py
+++ main.py
@@ -10,12 +10,8 @@ def read_root():
     time.sleep(5)
     process_id = os.getpid()
     thread_id = threading.get_ident()
-    print(f"Task processed in Process ID: {process_id}, Thread ID: {thread_id}")
     return {"message": "Hello, World!", "process_id": process_id, "thread_id": thread_id}
 @app.get('/health')
 def health():
-    process_id = os.getpid()
-    thread_id = threading.get_ident()
-    print(f"Task processed in Process ID: {process_id}, Thread ID: {thread_id}")
     return {'message': 'I am working'}
     
