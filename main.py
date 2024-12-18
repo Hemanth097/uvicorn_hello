0f76cdfc45d80054d285dca6daf2caa73203a129
Hemanth Vudumula <vudumulahemanth@gmail.com>
2024-12-18 16:12:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     time.sleep(5)
-    return {'message': 'Hello, World!'}
+    process_id = os.getpid()
+    thread_id = threading.get_ident()
+    print(f"Task processed in Process ID: {process_id}, Thread ID: {thread_id}")
+    return {"message": "Hello, World!", "process_id": process_id, "thread_id": thread_id}
 @app.get('/health')
-def read_root():
+def health():
+    process_id = os.getpid()
+    thread_id = threading.get_ident()
+    print(f"Task processed in Process ID: {process_id}, Thread ID: {thread_id}")
     return {'message': 'I am working'}
+    
