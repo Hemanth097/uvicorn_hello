a076aecb444bcff5dd075a3bb961d3dcb9d682c7
Hemanth Vudumula <vudumulahemanth@gmail.com>
2024-12-18 15:45:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     time.sleep(5)
-    return {'message': 'Hello, World!'}"
+    return {'message': 'Hello, World!'}
 @app.get('/health')
 def read_root():
-    return {'message': 'I am working'}"
+    return {'message': 'I am working'}
